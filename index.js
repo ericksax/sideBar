9a130f5a86868f50ae08fc910e35233e35373ee1
ericksax <erickfreitas@gmail.com>
2022-02-02 12:31:12 -0300

    added style changes

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const nave = document.querySelector("nav")
-const burguerBtn = document.querySelector(".burguerMenu")
+const nave = document.querySelector("nav");
+const burguerBtn = document.querySelector(".burguerMenu");
+let boxSearch = document.querySelector("#boxSearch");
+let input = document.querySelector("#input");
 
+// boxSearch.addEventListener("click", () => {
+//   let openMenu = document.querySelector(".open_menu");
+//   if (openMenu) {
+//     input.classList.add("search");
+//   }
+
+//   console.log(input, openMenu);
+// });
 
 burguerBtn.addEventListener("click", () => {
-    nave.classList.toggle("open_menu")
-})
+  //   input.classList.remove("search");
+
+  input.classList.add("search");
+  nave.classList.toggle("open_menu");
+});
